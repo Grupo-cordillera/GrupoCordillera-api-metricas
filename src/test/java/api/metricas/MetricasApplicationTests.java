fb834408ea7cb6a71f7cbfdc9511d4645f645da1
RenatoHinojosa <ren.hinojosa@duocuc.cl>
2026-07-01 16:13:49 -0400

    first commit

---
+++ src/test/java/api/metricas/MetricasApplicationTests.java
@@ -0,0 +1,13 @@
+package api.metricas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
